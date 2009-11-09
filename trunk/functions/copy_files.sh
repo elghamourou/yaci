f7f76205c9b783d21938dc0c23e023a54491cb54
tdhooge@harco.com <tdhooge@harco.com@74acc3ac-6f5b-11de-83e1-4fc93b4a6815>
2009-11-09 21:29:41 +0000

    blah

--- trunk/functions/copy_files.sh
+++ trunk/functions/copy_files.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
-
-###############################################################################
-# $URL$
-# $Author$
-# $Date$
-# $Rev$
-###############################################################################
+################################################################################
+#  Copyright (C) 2001-2002 The Regents of the University of California.
+#  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+#  Written by Makia Minich <makia@llnl.gov> and Trent D'Hooge <tdhooge@llnl.gov>
+#  UCRL-CODE-2003-024.
+#  
+#  This file is part of YACI, a cluster installation tool.
+#  For details, see <http://www.llnl.gov/linux/yaci/>.
+#  
+#  YACI is free software; you can redistribute it and/or modify it under
+#  the terms of the GNU General Public License as published by the Free
+#  Software Foundation; either version 2 of the License, or (at your option)
+#  any later version.
+#  
+#  YACI is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+#  details.
+#  
+#  You should have received a copy of the GNU General Public License along
+#  with YACI; if not, write to the Free Software Foundation, Inc.,
+#  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+################################################################################
 
 # Copy files or directories into image directory
 
