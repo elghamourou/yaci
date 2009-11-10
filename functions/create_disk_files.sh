2829924cac745e4dd4f99375b160f4b16de60a69
tdhooge <tdhooge@harco.com>
2009-11-10 20:37:54 +0000

    more updates

--- functions/create_disk_files.sh
+++ functions/create_disk_files.sh
@@ -1,33 +1,13 @@
 #!/bin/bash
-################################################################################
-# create_disk_files,v 1.21 2004/10/28 18:08:26 tdhooge Exp
-################################################################################
-#  Copyright (C) 2001-2002 The Regents of the University of California.
-#  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
-#  Written by Makia Minich <makia@llnl.gov> and Trent D'Hooge <tdhooge@llnl.gov>
-#  UCRL-CODE-2003-024.
-#  
-#  This file is part of YACI, a cluster installation tool.
-#  For details, see <http://www.llnl.gov/linux/yaci/>.
-#  
-#  YACI is free software; you can redistribute it and/or modify it under
-#  the terms of the GNU General Public License as published by the Free
-#  Software Foundation; either version 2 of the License, or (at your option)
-#  any later version.
-#  
-#  YACI is distributed in the hope that it will be useful, but WITHOUT ANY
-#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-#  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-#  details.
-#  
-#  You should have received a copy of the GNU General Public License along
-#  with YACI; if not, write to the Free Software Foundation, Inc.,
-#  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
-################################################################################
-#
-# create_disk_files
+
+###############################################################################
+# $URL: https://yaci.googlecode.com/svn/trunk/functions/disk.sh $
+# $Author: tdhooge@harco.com $
+# $Date: 2009-11-09 13:17:20 -0800 (Mon, 09 Nov 2009) $
+# $Rev: 15 $
+###############################################################################
+
 # Parse the partition list and create a useable fstab and sfdisk file.
-#
 
 PATH=/sbin:/bin:/usr/bin:/usr/sbin:/tmp
 
@@ -249,6 +229,14 @@ create_disk_files()
 # 4) bootable?
 # 5) partition number
 # 6) stype
+
+if [ ${#} -lt 1 ]; then
+  echo "USAGE: $0 image"
+  return 1;
+fi
+
+# image name
+local image=$1
 
 start=0
 end=0
